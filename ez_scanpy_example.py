f50029805e9403e046594709641fd211d141f24e
Richard Perez <richard.perez@ucsf.edu>
2018-09-10 18:49:49 -0700

    Add files via upload

--- ez_scanpy_example.py
+++ ez_scanpy_example.py
@@ -6,21 +6,21 @@ import ez_scanpy as ezsc
 # Remove doublets and create initial .h5ad object #
 ###################################################
 h5csvpath = '/ye/yelabstore2/10x.lupus/eqtls/demux.v2/v2.batches.h5.csv'
-no_norm = '/ye/yelabstore2/10x.lupus/process.scRNAseq/Richard/PlottingScripts/ez_scanpy/SLEcrossX_nonorm.h5ad'
+no_norm = 'SLEcrossX_nonorm.h5ad'
 #ezsc.remove_doublets(h5csvpath, no_norm)
 
 
 #########################################
 # Basic processing and remove platelets #
 #########################################
-processed = '/ye/yelabstore2/10x.lupus/process.scRNAseq/Richard/PlottingScripts/ez_scanpy/SLEcrossX_processed.h5ad'
+processed = 'SLEcrossX_processed.h5ad'
 #ezsc.basicprocessing_noplatelets(no_norm, processed)
 
 
 ###################################################
 # Basic processing and without removing platelets #
 ###################################################
-processed_plat = '/ye/yelabstore2/10x.lupus/process.scRNAseq/Richard/PlottingScripts/ez_scanpy/SLEcrossX_processed_with_platelets.h5ad'
+processed_plat = 'SLEcrossX_processed_with_platelets.h5ad'
 #ezsc.basic_processing(no_norm, processed_plat)
 
 
@@ -48,23 +48,23 @@ annotation = {'CD8+ T': ['CD8A', 'CD8B', 'CD3D'], 'CD4+ T': ['CD3D', 'ANK3', 'IL
 #     Subpopulation analysis    #
 # Must run basic analysis first #
 #################################
-Mpath = '/ye/yelabstore2/10x.lupus/process.scRNAseq/Richard/PlottingScripts/ez_scanpy/Mono.h5ad'
-Bpath = '/ye/yelabstore2/10x.lupus/process.scRNAseq/Richard/PlottingScripts/ez_scanpy/Bcell.h5ad'
-T4path = '/ye/yelabstore2/10x.lupus/process.scRNAseq/Richard/PlottingScripts/ez_scanpy/T4cell.h5ad'
-T8path = '/ye/yelabstore2/10x.lupus/process.scRNAseq/Richard/PlottingScripts/ez_scanpy/T8cell.h5ad'
-NKpath = '/ye/yelabstore2/10x.lupus/process.scRNAseq/Richard/PlottingScripts/ez_scanpy/NK.h5ad'
-DCpath = '/ye/yelabstore2/10x.lupus/process.scRNAseq/Richard/PlottingScripts/ez_scanpy/DC.h5ad'
-ezsc.subpopulation_analysis(cell_type_IDs=['CD14 Mo', 'CD16 Mo'], filepath=processed, no_norm_filepath=no_norm, savepath=Mpath)
-ezsc.subpopulation_analysis(cell_type_IDs=['CD4+ T'], filepath=processed, no_norm_filepath=no_norm, savepath=T4path)
-ezsc.subpopulation_analysis(cell_type_IDs=['CD8+ T'], filepath=processed, no_norm_filepath=no_norm, savepath=T8path)
-ezsc.subpopulation_analysis(cell_type_IDs=['NK'], filepath=processed, no_norm_filepath=no_norm, savepath=NKpath)
-ezsc.subpopulation_analysis(cell_type_IDs=['B'], filepath=processed, no_norm_filepath=no_norm, savepath=Bpath)
-ezsc.subpopulation_analysis(cell_type_IDs=['cDC', 'pDC'], filepath=processed, no_norm_filepath=no_norm, savepath=DCpath)
+Mpath = 'Mono.h5ad'
+Bpath = 'Bcell.h5ad'
+T4path = 'T4cell.h5ad'
+T8path = 'T8cell.h5ad'
+NKpath = 'NK.h5ad'
+DCpath = 'DC.h5ad'
+#ezsc.subpopulation_analysis(cell_type_IDs=['CD14 Mo', 'CD16 Mo'], filepath=processed, no_norm_filepath=no_norm, savepath=Mpath)
+#ezsc.subpopulation_analysis(cell_type_IDs=['CD4+ T'], filepath=processed, no_norm_filepath=no_norm, savepath=T4path)
+#ezsc.subpopulation_analysis(cell_type_IDs=['CD8+ T'], filepath=processed, no_norm_filepath=no_norm, savepath=T8path)
+#ezsc.subpopulation_analysis(cell_type_IDs=['NK'], filepath=processed, no_norm_filepath=no_norm, savepath=NKpath)
+#ezsc.subpopulation_analysis(cell_type_IDs=['B'], filepath=processed, no_norm_filepath=no_norm, savepath=Bpath)
+#ezsc.subpopulation_analysis(cell_type_IDs=['cDC', 'pDC'], filepath=processed, no_norm_filepath=no_norm, savepath=DCpath)
+
 
 
 ##########################################
 # Subpopulation analysis regress out IFN #
 ##########################################
-Mpath = '/ye/yelabstore2/10x.lupus/process.scRNAseq/Richard/PlottingScripts/ez_scanpy/Mono.regressIFN.h5ad'
-IFN = '/ye/yelabstore2/10x.lupus/process.scRNAseq/Richard/PlottingScripts/ez_scanpy/IFN.csv'
-ezsc.subpopulation_analysis_regressIFN(cell_type_IDs=['CD14 Mo', 'CD16 Mo'], filepath=processed, no_norm_filepath=no_norm, IFNpath=IFN, savepath=Mpath)
+Mpath = 'Mono.regressIFN.h5ad'
+#ezsc.subpopulation_analysis_regressIFN(cell_type_IDs=['CD14 Mo', 'CD16 Mo'], filepath=processed, no_norm_filepath=no_norm, savepath=Mpath)
