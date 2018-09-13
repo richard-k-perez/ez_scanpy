eb5e90b2edbf0a3abaa3fa16925192fb6f37a6b5
Richard Perez <richard.perez@ucsf.edu>
2018-09-13 11:15:50 -0700

    Add files via upload

--- ez_scanpy.py
+++ ez_scanpy.py
@@ -35,9 +35,9 @@ def remove_doublets(h5csvpath, savepath):
     logging.info(str('Data structure details: ' + str(adata_singlet)))
     clf = doubletdetection.BoostClassifier()
     # raw_counts is a cells by genes count matrix
-    labels = clf.fit(adata_singlet.X).predict()
+    labels = clf.fit(adata_singlet.X).predict(voter_thresh=0.70)
     f2, tsne_coords, clusters = doubletdetection.plot.tsne(adata_singlet.X, labels, random_state=1, save='tsne_test.pdf', show=False)
-
+    f3 = doubletdetection.plot.threshold(clf, save='threshold_test.pdf', show=False, p_step=6)
     logging.info(str(labels))
     nan_index = np.isnan(labels)
     labels[nan_index] = 1
@@ -68,7 +68,7 @@ def remove_doublets(h5csvpath, savepath):
         logging.info(str('Data structure details: ' + str(bdata_singlet)))
         clf = doubletdetection.BoostClassifier()
         # raw_counts is a cells by genes count matrix
-        labels = clf.fit(bdata_singlet.X).predict()
+        labels = clf.fit(bdata_singlet.X).predict(voter_thresh=0.70)
         f2, tsne_coords, clusters = doubletdetection.plot.tsne(bdata_singlet.X, labels, random_state=1, save=str(str(i) + 'b_tsne_test.pdf'), show=False)
 
         logging.info(str(labels))
@@ -90,6 +90,115 @@ def remove_doublets(h5csvpath, savepath):
     # Back the file to this directory for easy memory access
     adata_singlet.filename = 'backed.h5ad'
     adata_singlet.write(savepath)
+    adata.write_csvs(savepath.split('.')[0])
+
+
+def remove_doublets_flare(flarepaths, flaregps, bestfilepath, savepath):
+    import numpy as np
+    import pandas as pd
+    import scanpy.api as sc
+    import logging
+    import doubletdetection
+
+    ##################
+    # Configure file #
+    ##################
+    sc.settings.verbosity = 2
+    sc.settings.autoshow = False
+    logging.basicConfig(level=logging.INFO)
+
+
+    ############################
+    # Compile demuxlet results #
+    ############################
+    for ii in range(len(flarepaths)):
+        best = pd.read_csv(bestfilepath[ii], delimiter='\t')
+        singlets_ind = []
+        singlets_bc = []
+        for jj in range(len(best['BEST'].values)):
+            if 'SNG' in best['BEST'].values[jj]:
+                singlets_bc.append(best['BARCODE'].values[jj])
+                singlets_ind.append(best['BEST'].values[jj])
+
+        if ii == 0:
+            adata = sc.read_10x_h5(flarepaths[ii])
+            adata = adata[adata.obs_names.isin(singlets_bc)]
+            logging.info(str(adata))
+            singlets_bc = pd.Series(singlets_bc)
+            singlets_ind = pd.Series(singlets_ind)
+            logging.info(str(singlets_bc.isin(adata.obs_names.tolist()).values))
+            singlets_ind = singlets_ind[singlets_bc.isin(adata.obs_names.tolist()).values]
+            adata.obs['ind_cov'] = singlets_ind.values
+            adata.obs['batch_cov'] = np.repeat(flaregps[ii], len(adata.obs_names))
+            logging.info(str('Adata.X: ' + str(np.shape(adata.X))))
+
+            # Independent Doublet Detection
+            clf = doubletdetection.BoostClassifier()
+            # raw_counts is a cells by genes count matrix
+            labels = clf.fit(adata.X).predict(voter_thresh=0.70)
+            f2, tsne_coords, clusters = doubletdetection.plot.tsne(adata.X, labels, random_state=1,
+                                                                   save='tsne_test.pdf', show=False)
+            logging.info(str(labels))
+            nan_index = np.isnan(labels)
+            labels[nan_index] = 1
+            logging.info(str('Number of Doublets: ' + str(np.sum(labels))))
+            labels = labels - 1
+            logging.info(str(labels))
+            labels = np.abs(labels) > 0
+            logging.info(str(labels))
+            adata = adata[labels]
+            logging.info(str('Data structure details: ' + str(adata)))
+            f = doubletdetection.plot.convergence(clf, save='convergence_test.pdf', show=False)
+
+        else:
+            bdata = sc.read_10x_h5(flarepaths[ii])
+            bdata = bdata[bdata.obs_names.isin(singlets_bc)]
+            logging.info(str(bdata))
+            singlets_bc = pd.Series(singlets_bc)
+            singlets_ind = pd.Series(singlets_ind)
+            singlets_ind = singlets_ind[singlets_bc.isin(bdata.obs_names.tolist()).values]
+            bdata.obs['ind_cov'] = singlets_ind.values
+            bdata.obs['batch_cov'] = np.repeat(flaregps[ii], len(bdata.obs_names))
+
+            # Independent Doublet Detection
+            clf = doubletdetection.BoostClassifier()
+            # raw_counts is a cells by genes count matrix
+            labels = clf.fit(bdata.X).predict(voter_thresh=0.70)
+            f2, tsne_coords, clusters = doubletdetection.plot.tsne(bdata.X, labels, random_state=1,
+                                                                   save=str(str(ii) + 'b_tsne_test.pdf'), show=False)
+            logging.info(str(labels))
+            nan_index = np.isnan(labels)
+            labels[nan_index] = 1
+            logging.info(str('Number of Doublets: ' + str(np.sum(labels))))
+            labels = labels - 1
+            logging.info(str(labels))
+            labels = np.abs(labels) > 0
+            logging.info(str(labels))
+            bdata = bdata[labels]
+            logging.info(str('Data structure details: ' + str(bdata)))
+            f = doubletdetection.plot.convergence(clf, save=str(str(ii) + 'b_convergence_test.pdf'), show=False)
+
+            # Concatenate files.
+            adata = adata.concatenate(bdata)
+
+    # Add disease state labels to observations
+    IDpath = '/ye/yelabstore2/10x.lupus/process.scRNAseq/Richard/PlottingScripts/Flare/all_IDs.csv'
+    IDmap = pd.read_csv(IDpath)
+    disease_cov = []
+    ind_cov = []
+    for ii in range(len(adata.obs_names)):
+        tmpmap = IDmap[IDmap['Run'].isin([adata.obs['batch_cov'].values[ii][0]])]
+        for jj in range(len(tmpmap['Grep for this1'].values)):
+            if tmpmap['Grep for this1'].values[jj] in adata.obs['ind_cov'].values[ii]:
+                disease_cov.append(tmpmap['Type'].values[jj])
+                ind_cov.append(tmpmap['Grep for this1'].values[jj])
+    adata.obs['disease_cov'] = disease_cov
+    # Replace verbose names with succinct names
+    adata.obs['ind_cov'] = ind_cov
+    logging.info(str(adata))
+    logging.info('Saving compiled demuxlet results')
+    adata.write(savepath)
+    adata.write_csvs(savepath.split('.')[0])
 
 
 def basic_processing(filepath, savepath):
@@ -159,6 +268,7 @@ def basic_processing(filepath, savepath):
     adata.uns['total_pbmcs'] = total_pbmcs
     logging.info('Saving processed data')
     adata.write(savepath)
+    adata.write_csvs(savepath.split('.')[0])
 
 
 def basicprocessing_noplatelets(filepath, savepath):
@@ -242,6 +352,7 @@ def basicprocessing_noplatelets(filepath, savepath):
     adata.uns['total_pbmcs'] = total_pbmcs
     logging.info('Saving processed data')
     adata.write(savepath)
+    adata.write_csvs(savepath.split('.')[0])
 
 
 def cell_identity(filepath, annotation):
@@ -307,7 +418,7 @@ def basic_analysis(filepath):
     # Set parameters
     intialization = 1
     n_components = 20
-    resolution = 1
+    resolution = 0.5
     # Run louvain clustering on theoretical future gene expression per cell
     logging.info('Estimating louvain cluster identities for gene expression values.')
     sc.pp.pca(adata, random_state=intialization)
@@ -327,6 +438,8 @@ def basic_analysis(filepath):
     adata.obs['ordering_UMAP'] = sc.pp.pca(adata.obsm['X_umap'], n_comps=1, copy=True)
     logging.info('UMAP ordering complete.')
     sc.tl.rank_genes_groups(adata, groupby='louvain', method='wilcoxon')
+    logging.info('Processing PHATE')
+    sc.tl.phate(adata, n_pca=10)
     logging.info('Ranked genes complete.')
     adata.write(filepath)
     logging.info('Basic analysis complete.')
@@ -341,6 +454,7 @@ def getIFNscore(adata):
     sc.tl.pca(adata_ifn)
     adata.obs['IFN'] = adata_ifn.obsm['X_pca'][:, 1]
     return adata
+
 
 def subpopulation_analysis(cell_type_IDs, filepath, no_norm_filepath, savepath):
     import scanpy.api as sc
@@ -412,9 +526,9 @@ def subpopulation_analysis(cell_type_IDs, filepath, no_norm_filepath, savepath):
     adata.write(savepath)
     # Basic analysis
     basic_analysis(savepath)
+    adata.write_csvs(savepath.split('.')[0])
 
-
-def subpopulation_analysis_regressIFN(cell_type_IDs, filepath, no_norm_filepath, IFNpath, savepath):
+def subpopulation_analysis_regressIFN(cell_type_IDs, filepath, no_norm_filepath, savepath):
     import scanpy.api as sc
     import logging
     import numpy as np
@@ -487,4 +601,159 @@ def subpopulation_analysis_regressIFN(cell_type_IDs, filepath, no_norm_filepath,
     logging.info('Basic processing complete.')
     # Basic analysis
     basic_analysis(savepath)
+    adata.write_csvs(savepath.split('.')[0])
 
+def subpopulation_analysis_covariate(cell_type_IDs, covariate, covariate_grp, filepath, no_norm_filepath, savepath):
+    import scanpy.api as sc
+    import logging
+    import numpy as np
+
+    ####################
+    #  Configure file  #
+    ####################
+    # Configure logging
+    logging.basicConfig(level=logging.INFO)
+    # Configure scanpy settings
+    sc.settings.verbosity = 2
+    sc.settings.autoshow = False
+
+    ####################
+    # Basic processing #
+    ####################
+    adata = sc.read(filepath)
+    logging.info(str('Processed data structure details: ' + str(adata)))
+    # Extract estimated louvain groups from PBMC clustering.
+    louvain = adata.obs['louvain'].tolist()
+    ct_cov = adata.obs['ct_cov'].tolist()
+    cellID = adata.obs_names.tolist()
+    adata = sc.read(no_norm_filepath)
+    logging.info(str('No normalization data structure details: ' + str(adata)))
+    adata = adata[adata.obs_names.isin(cellID)]
+    adata.obs['louvain'] = louvain
+    adata.obs['ct_cov'] = ct_cov
+
+    logging.info(str('New data structure details: ' + str(adata)))
+    bdata = adata[adata.obs['ct_cov'].isin(cell_type_IDs)].copy(savepath)
+    bdata = bdata[bdata.obs[covariate] == covariate_grp]
+    bdata.write(savepath)
+
+    ######################
+    # Further processing #
+    ######################
+    adata = sc.read(savepath)
+    adata.obs['batchcore'] = adata.obs['batch_cov'].astype('category')
+    adata.var_names_make_unique()
+    # Extract list of genes
+    genelist = adata.var_names.tolist()
+    # Find mitochondrial genes
+    mito_genes_names = [gn for gn in genelist if gn.startswith('MT-')]
+    logging.info(str('Mito genes: ' + str(mito_genes_names)))
+    # Find indices of mitochondrial genes
+    mito_genes = [genelist.index(gn) for gn in mito_genes_names]
+    # For each cell compute fraction of counts in mito genes vs. all genes
+    adata.obs['percent_mito'] = np.ravel(np.sum(adata[:, mito_genes].X, axis=1)) / np.ravel(np.sum(adata.X, axis=1))
+    # Add the total counts per cell as observations-annotation to adata
+    adata.obs['n_counts'] = np.ravel(adata.X.sum(axis=1))
+    logging.info('Saving raw and raw counts')
+    adata.uns['raw_counts'] = adata.X
+    adata.raw = sc.pp.log1p(adata, copy=True)
+    logging.info('Normalizing total counts to 10,000')
+    sc.pp.normalize_per_cell(adata, counts_per_cell_after=1e4)
+    logging.info('Filtering genes')
+    filter_result = sc.pp.filter_genes_dispersion(adata.X, max_mean=5, min_disp=0.25, min_mean=0.05, log=True)
+    adata = adata[:, filter_result.gene_subset]
+    logging.info('Log transforming data')
+    sc.pp.log1p(adata)
+    logging.info(str('Data structure details: ' + str(adata)))
+    logging.info('Regressing out variance within total nUMIs and % mitochondrial UMIs')
+    sc.pp.regress_out(adata, ['n_counts', 'percent_mito'])
+    logging.info('Batch correcting by regressing out batch variance')
+    sc.pp.regress_out(adata, ['batchcore'])
+    logging.info('Scaling expression data')
+    sc.pp.scale(adata, max_value=10)
+    logging.info('Basic processing complete.')
+    adata.write(savepath)
+    # Basic analysis
+    basic_analysis(savepath)
+    adata.write_csvs(savepath.split('.')[0])
+
+
+def subpopulation_analysis_removeIFNGenes(cell_type_IDs, filepath, no_norm_filepath, savepath):
+    import scanpy.api as sc
+    import logging
+    import numpy as np
+    import pandas as pd
+
+    ####################
+    #  Configure file  #
+    ####################
+    # Configure logging
+    logging.basicConfig(level=logging.INFO)
+    # Configure scanpy settings
+    sc.settings.verbosity = 2
+    sc.settings.autoshow = False
+
+    ####################
+    # Basic processing #
+    ####################
+    adata = sc.read(filepath)
+    logging.info(str('Processed data structure details: ' + str(adata)))
+    # Extract estimated louvain groups from PBMC clustering.
+    louvain = adata.obs['louvain'].tolist()
+    ct_cov = adata.obs['ct_cov'].tolist()
+    cellID = adata.obs_names.tolist()
+    adata = sc.read(no_norm_filepath)
+    logging.info(str('No normalization data structure details: ' + str(adata)))
+    adata = adata[adata.obs_names.isin(cellID)]
+    adata.obs['louvain'] = louvain
+    adata.obs['ct_cov'] = ct_cov
+    logging.info(str('New data structure details: ' + str(adata)))
+    bdata = adata[adata.obs['ct_cov'].isin(cell_type_IDs)].copy(savepath)
+    bdata.write(savepath)
+
+    ######################
+    # Further processing #
+    ######################
+    adata = sc.read(savepath)
+    adata.obs['batchcore'] = adata.obs['batch_cov'].astype('category')
+    adata.var_names_make_unique()
+    # Extract list of genes
+    genelist = adata.var_names.tolist()
+    # Find mitochondrial genes
+    mito_genes_names = [gn for gn in genelist if gn.startswith('MT-')]
+    logging.info(str('Mito genes: ' + str(mito_genes_names)))
+    # Find indices of mitochondrial genes
+    mito_genes = [genelist.index(gn) for gn in mito_genes_names]
+    # For each cell compute fraction of counts in mito genes vs. all genes
+    adata.obs['percent_mito'] = np.ravel(np.sum(adata[:, mito_genes].X, axis=1)) / np.ravel(np.sum(adata.X, axis=1))
+    # Add the total counts per cell as observations-annotation to adata
+    adata.obs['n_counts'] = np.ravel(adata.X.sum(axis=1))
+    logging.info('Saving raw and raw counts')
+    adata.uns['raw_counts'] = adata.X
+    adata.raw = sc.pp.log1p(adata, copy=True)
+    logging.info('Normalizing total counts to 10,000')
+    sc.pp.normalize_per_cell(adata, counts_per_cell_after=1e4)
+    logging.info('Filtering genes')
+    filter_result = sc.pp.filter_genes_dispersion(adata.X, max_mean=5, min_disp=0.25, min_mean=0.05, log=True)
+    adata = adata[:, filter_result.gene_subset]
+    logging.info('Log transforming data')
+    sc.pp.log1p(adata)
+    logging.info(str('Data structure details: ' + str(adata)))
+    logging.info('Regressing out variance within total nUMIs and % mitochondrial UMIs')
+    sc.pp.regress_out(adata, ['n_counts', 'percent_mito'])
+    logging.info('Batch correcting by regressing out batch variance')
+    sc.pp.regress_out(adata, ['batchcore'])
+    logging.info('Scaling expression data')
+    sc.pp.scale(adata, max_value=10)
+    logging.info('Basic processing complete.')
+    logging.info(str('Data structure before removing genes: ' + str(adata)))
+    data = pd.read_csv('/ye/yelabstore/10x.lupus/batch2.analysis/v2.diff.exp/cd14.sig.txt', sep='\t')
+    data = data['featureData.symbol'].values.tolist()
+    logging.info(str('Removing the following genes: ' + str(data)))
+    Genes = pd.Series(adata.var_names.tolist())
+    adata = adata[:, Genes.isin(data) == False]
+    logging.info(str('Data structure after removing genes: ' + str(adata)))
+    adata.write(savepath)
+    # Basic analysis
+    basic_analysis(savepath)
+    adata.write_csvs(savepath.split('.')[0])
--- ez_scanpy_example.py
+++ ez_scanpy_example.py
@@ -7,28 +7,59 @@ import ez_scanpy as ezsc
 ###################################################
 h5csvpath = '/ye/yelabstore2/10x.lupus/eqtls/demux.v2/v2.batches.h5.csv'
 no_norm = 'SLEcrossX_nonorm.h5ad'
-#ezsc.remove_doublets(h5csvpath, no_norm)
+ezsc.remove_doublets(h5csvpath, no_norm)
+
+
+#########################################################
+# Remove doublets and create initial .h5ad object flare #
+#########################################################
+no_norm_flare = 'SLE_flare_nonorm.h5ad'
+flarepaths = [
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_3/AbFlare-3_1/outs/filtered_gene_bc_matrices_h5.h5',
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_3/AbFlare-3_2/outs/filtered_gene_bc_matrices_h5.h5',
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_3/AbFlare-3_3/outs/filtered_gene_bc_matrices_h5.h5',
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_3/AbFlare-3_4/outs/filtered_gene_bc_matrices_h5.h5',
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_4/AbFlare-4_1/outs/filtered_gene_bc_matrices_h5.h5',
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_4/AbFlare-4_2/outs/filtered_gene_bc_matrices_h5.h5',
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_4/AbFlare-4_3/outs/filtered_gene_bc_matrices_h5.h5',
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_4/AbFlare-4_4/outs/filtered_gene_bc_matrices_h5.h5']
+
+flaregroups = ['3-1', '3-2', '3-3', '3-4', '4-1', '4-2', '4-3', '4-4']
+demuxletpaths = [
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_3/AbFlare-3_1/demux_out_raw_oldvcfs/demuxlet.raw.flare3samp.best',
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_3/AbFlare-3_2/demux_out_raw_oldvcfs/demuxlet.raw.flare3samp.best',
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_3/AbFlare-3_3/demux_out_raw_oldvcfs/demuxlet.raw.flare3samp.best',
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_3/AbFlare-3_4/demux_out_raw_oldvcfs/demuxlet.raw.flare3samp.best',
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_4/AbFlare-4_1/demux_out_raw_oldvcfs/demuxlet.raw.flare4samps.best',
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_4/AbFlare-4_2/demux_out_raw_oldvcfs/demuxlet.raw.flare4samps.best',
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_4/AbFlare-4_3/demux_out_raw_oldvcfs/demuxlet.raw.flare4samps.best',
+        '/ye/yelabstore2/george/Sasha_Flare_Studies/10xcount/run_4/AbFlare-4_4/demux_out_raw_oldvcfs/demuxlet.raw.flare4samps.best']
+
+ezsc.remove_doublets_flare(flarepaths=flarepaths, flaregps=flaregroups, bestfilepath=demuxletpaths, savepath=no_norm_flare)
+
 
 
 #########################################
 # Basic processing and remove platelets #
 #########################################
 processed = 'SLEcrossX_processed.h5ad'
-#ezsc.basicprocessing_noplatelets(no_norm, processed)
+ezsc.basicprocessing_noplatelets(no_norm, processed)
 
 
 ###################################################
-# Basic processing and without removing platelets #
+# Basic processing and with platelets             #
 ###################################################
 processed_plat = 'SLEcrossX_processed_with_platelets.h5ad'
-#ezsc.basic_processing(no_norm, processed_plat)
-
+processed_plat_flare = 'SLEcrossX_processed_with_platelets_flare.h5ad'
+ezsc.basic_processing(no_norm, processed_plat)
+ezsc.basic_processing(no_norm_flare, processed_plat_flare)
 
 ##################
 # Basic analysis #
 ##################
-#ezsc.basic_analysis(processed)
-#ezsc.basic_analysis(processed_plat)
+ezsc.basic_analysis(processed)
+ezsc.basic_analysis(processed_plat)
+ezsc.basic_analysis(processed_plat_flare)
 
 
 ##########################################
@@ -40,8 +71,9 @@ annotation = {'CD8+ T': ['CD8A', 'CD8B', 'CD3D'], 'CD4+ T': ['CD3D', 'ANK3', 'IL
               'B': ['CD19', 'MS4A1', 'CD79A'], 'NK': ['KLRF1', 'GNLY', 'NKG7'],
               'cDC': ['HLA.DQA1', 'FCER1A', 'GPR183'], 'pDC': ['CLEC4C', 'TSPAN13', 'IGJ'],
               'MK': ['PPBP', 'GNG11']}
-#ezsc.cell_identity(processed, annotation)  # Must run 'Basic analysis' first
-#ezsc.cell_identity(processed_plat, annotation)  # Must run 'Basic analysis' first
+ezsc.cell_identity(processed, annotation)  # Must run 'Basic analysis' first
+ezsc.cell_identity(processed_plat, annotation)  # Must run 'Basic analysis' first
+ezsc.cell_identity(processed_plat_flare, annotation)  # Must run 'Basic analysis' first
 
 
 #################################
@@ -54,17 +86,56 @@ T4path = 'T4cell.h5ad'
 T8path = 'T8cell.h5ad'
 NKpath = 'NK.h5ad'
 DCpath = 'DC.h5ad'
-#ezsc.subpopulation_analysis(cell_type_IDs=['CD14 Mo', 'CD16 Mo'], filepath=processed, no_norm_filepath=no_norm, savepath=Mpath)
-#ezsc.subpopulation_analysis(cell_type_IDs=['CD4+ T'], filepath=processed, no_norm_filepath=no_norm, savepath=T4path)
-#ezsc.subpopulation_analysis(cell_type_IDs=['CD8+ T'], filepath=processed, no_norm_filepath=no_norm, savepath=T8path)
-#ezsc.subpopulation_analysis(cell_type_IDs=['NK'], filepath=processed, no_norm_filepath=no_norm, savepath=NKpath)
-#ezsc.subpopulation_analysis(cell_type_IDs=['B'], filepath=processed, no_norm_filepath=no_norm, savepath=Bpath)
-#ezsc.subpopulation_analysis(cell_type_IDs=['cDC', 'pDC'], filepath=processed, no_norm_filepath=no_norm, savepath=DCpath)
+MKpath = 'MK.h5ad'
+ezsc.subpopulation_analysis(cell_type_IDs=['CD14 Mo', 'CD16 Mo'], filepath=processed_plat, no_norm_filepath=no_norm, savepath=Mpath)
+ezsc.subpopulation_analysis(cell_type_IDs=['CD4+ T'], filepath=processed_plat, no_norm_filepath=no_norm, savepath=T4path)
+ezsc.subpopulation_analysis(cell_type_IDs=['CD8+ T'], filepath=processed_plat, no_norm_filepath=no_norm, savepath=T8path)
+ezsc.subpopulation_analysis(cell_type_IDs=['NK'], filepath=processed_plat, no_norm_filepath=no_norm, savepath=NKpath)
+ezsc.subpopulation_analysis(cell_type_IDs=['B'], filepath=processed_plat, no_norm_filepath=no_norm, savepath=Bpath)
+ezsc.subpopulation_analysis(cell_type_IDs=['cDC', 'pDC'], filepath=processed_plat, no_norm_filepath=no_norm, savepath=DCpath)
+ezsc.subpopulation_analysis(cell_type_IDs=['MK'], filepath=processed_plat, no_norm_filepath=no_norm, savepath=MKpath)
 
+
+#################################
+# Subpopulation analysis flare  #
+# Must run basic analysis first #
+#################################
+Mpath = 'Mono_flare.h5ad'
+Bpath = 'Bcell_flare.h5ad'
+T4path = 'T4cell_flare.h5ad'
+T8path = 'T8cell_flare.h5ad'
+NKpath = 'NK_flare.h5ad'
+DCpath = 'DC_flare.h5ad'
+MKpath = 'MK_flare.h5ad'
+ezsc.subpopulation_analysis(cell_type_IDs=['CD14 Mo', 'CD16 Mo'], filepath=processed_plat_flare, no_norm_filepath=no_norm_flare, savepath=Mpath)
+ezsc.subpopulation_analysis(cell_type_IDs=['CD4+ T'], filepath=processed_plat_flare, no_norm_filepath=no_norm_flare, savepath=T4path)
+ezsc.subpopulation_analysis(cell_type_IDs=['CD8+ T'], filepath=processed_plat_flare, no_norm_filepath=no_norm_flare, savepath=T8path)
+ezsc.subpopulation_analysis(cell_type_IDs=['NK'], filepath=processed_plat_flare, no_norm_filepath=no_norm_flare, savepath=NKpath)
+ezsc.subpopulation_analysis(cell_type_IDs=['B'], filepath=processed_plat_flare, no_norm_filepath=no_norm_flare, savepath=Bpath)
+ezsc.subpopulation_analysis(cell_type_IDs=['cDC', 'pDC'], filepath=processed_plat_flare, no_norm_filepath=no_norm_flare, savepath=DCpath)
+ezsc.subpopulation_analysis(cell_type_IDs=['MK'], filepath=processed_plat_flare, no_norm_filepath=no_norm_flare, savepath=MKpath)
 
 
 ##########################################
 # Subpopulation analysis regress out IFN #
 ##########################################
-Mpath = 'Mono.regressIFN.h5ad'
-#ezsc.subpopulation_analysis_regressIFN(cell_type_IDs=['CD14 Mo', 'CD16 Mo'], filepath=processed, no_norm_filepath=no_norm, savepath=Mpath)
+Mpath = 'Mono_regressIFN.h5ad'
+ezsc.subpopulation_analysis_regressIFN(cell_type_IDs=['CD14 Mo', 'CD16 Mo'], filepath=processed_plat, no_norm_filepath=no_norm, savepath=Mpath)
+
+
+##########################################
+# Subpopulation analysis remove IFN      #
+##########################################
+Mpath = 'Mono_remove_IFNdiffexp.h5ad'
+ezsc.subpopulation_analysis_removeIFNGenes(cell_type_IDs=['CD14 Mo', 'CD16 Mo'], filepath=processed_plat, no_norm_filepath=no_norm, savepath=Mpath)
+Mpath = 'Mono_flare_remove_IFNdiffexp.h5ad'
+ezsc.subpopulation_analysis_removeIFNGenes(cell_type_IDs=['CD14 Mo', 'CD16 Mo'], filepath=processed_plat_flare, no_norm_filepath=no_norm_flare, savepath=Mpath)
+
+
+##########################################
+# Subpopulation analysis by dz covariate #
+##########################################
+Mpath_healthy = 'Mono_healthy.h5ad'
+Mpath_sle = 'Mono_sle.h5ad'
+ezsc.subpopulation_analysis_covariate(cell_type_IDs=['CD14 Mo', 'CD16 Mo'], covariate='disease_cov', covariate_grp= 'healthy', filepath=processed_plat, no_norm_filepath=no_norm, savepath=Mpath_healthy)
+ezsc.subpopulation_analysis_covariate(cell_type_IDs=['CD14 Mo', 'CD16 Mo'], covariate='disease_cov', covariate_grp= 'sle', filepath=processed_plat, no_norm_filepath=no_norm, savepath=Mpath_sle)
